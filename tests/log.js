7fc42054a21ac19d4938fe622e5b4b3ebc846d4f
Stephen Giles <steve@stephengil.es>
2017-11-24 18:01:12 +0000

    added additional .log() tests which check content of the log

--- tests/log.js
+++ tests/log.js
@@ -1,7 +1,6 @@
 let assert = require('assert'),
-  am = require('../am.js')
-
-var intercept = require('intercept-stdout'),
+  am = require('../am.js'),
+  intercept = require('intercept-stdout'),
   logs = [],
   unhook_intercept = intercept(function(txt) {
     logs.push(txt)
@@ -9,7 +8,9 @@ var intercept = require('intercept-stdout'),
 
 describe('log()', function() {
   describe('log resolved value', function() {
-    it('should return extended promise resolving to original resolved value ', function(done) {
+    it('should return extended promise resolving to original resolved value and log value', function(
+      done
+    ) {
       am([123, 456])
         .log()
         .wait(20)
@@ -23,8 +24,47 @@ describe('log()', function() {
         .catch(done)
     })
   })
+  describe('log resolved value and label', function() {
+    it('should return extended promise resolving to original resolved value and label', function(
+      done
+    ) {
+      am([123, 456])
+        .log('[log-test]')
+        .wait(20)
+        .then(function(r) {
+          assert.ok(
+            logs[logs.length - 1].trim().indexOf('123') !== -1 &&
+              logs[logs.length - 1].trim().indexOf('456') !== -1 &&
+              logs[logs.length - 1].trim().indexOf(['log-test']) !== -1
+          )
+          done()
+        })
+        .catch(done)
+    })
+  })
+  describe('log resolved value and line number', function() {
+    it('should return extended promise resolving to original resolved value and label', function(
+      done
+    ) {
+      am([123, 456])
+        .log('[log-test]', '', Error())
+        .wait(20)
+        .then(function(r) {
+          assert.ok(
+            logs[logs.length - 1].trim().indexOf('123') !== -1 &&
+              logs[logs.length - 1].trim().indexOf('456') !== -1 &&
+              logs[logs.length - 1].trim().indexOf('line 50') !== -1
+          )
+
+          done()
+        })
+        .catch(done)
+    })
+  })
   describe('log rejected value', function() {
-    it('should return extended promise rejecting to original resolved value', function(done) {
+    it('should return extended promise rejecting to original resolved value and log rejected value', function(
+      done
+    ) {
       am
         .reject({ error: 234 })
         .log()
@@ -34,6 +74,26 @@ describe('log()', function() {
           assert.ok(
             logs[logs.length - 1].trim().indexOf('error') !== -1 &&
               logs[logs.length - 1].trim().indexOf('234') !== -1
+          )
+          done()
+        })
+        .catch(done)
+    })
+  })
+  describe('log rejected value and label', function() {
+    it('should return extended promise rejecting to original rejected value and log label ', function(
+      done
+    ) {
+      am
+        .reject({ error: 234 })
+        .log('[resolve]', '[reject]')
+        .wait(20)
+        .catch(function(err) {
+          assert.deepStrictEqual(err, { error: 234 })
+          assert.ok(
+            logs[logs.length - 1].trim().indexOf('error') !== -1 &&
+              logs[logs.length - 1].trim().indexOf('234') !== -1 &&
+              logs[logs.length - 1].trim().indexOf('[reject]') !== -1
           )
           done()
         })
